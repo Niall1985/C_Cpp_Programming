8b4597ee2d9af5702fe9732d4df188cf24628230
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2024-10-07 22:15:30 +0530

    Update binary_tree_qts.c

--- binary_tree_qts.c
+++ binary_tree_qts.c
@@ -109,3 +109,135 @@ int main(){
     inorder(root);
     
 }
+
+
+
+
+// Q) Problem Statement
+
+// Divu wants a program to analyze a binary tree by traversing it in two different ways. 
+// The program begins by prompting for an integer n, indicating the number of nodes in the binary tree. Divu then provides n integers representing the values of these nodes. 
+// The program constructs the binary tree from the provided values and performs two types of traversals:
+
+// Forward In-order Traversal: It traverses the binary tree in order, printing each node's value as it visits.
+// Reverse In-order Traversal: It traverses the binary tree in order but prints each node's value in reverse order.
+// After conducting these traversals, the program prints the results.
+
+// Input format :
+// The first line consists of an integer n, representing the number of nodes in the binary tree.
+
+// The second line consists of n space-separated integers representing the values of the nodes in the binary tree.
+
+// Output format :
+// The first line prints the in-order traversal of the binary tree, where each node's value is printed as it is visited.
+
+// The second line prints the in-order traversal of the binary tree in reverse, where each node's value is printed in reverse order.
+
+// Refer to the sample output for the formatting specifications.
+
+// Code constraints :
+// The input integers should be unique.
+
+// 1 ≤ n ≤ 15
+
+// 1 ≤ Node values ≤ 50
+
+// Sample test cases :
+// Input 1 :
+// 5
+// 9 8 7 6 5
+// Output 1 :
+// 6 8 5 9 7 
+// 7 9 5 8 6 
+// Input 2 :
+// 8
+// 1 2 3 4 5 6 7 8
+// Output 2 :
+// 8 4 2 5 1 6 3 7 
+// 7 3 6 1 5 2 4 8 
+
+
+    
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *newNode(int data){
+    struct node *temp = (struct node*)malloc(sizeof(struct node));
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+
+struct node *insert(struct node *root, int data){
+    struct node *new_node = newNode(data);
+    if(root == NULL){
+        return new_node;
+    }
+    
+    struct node *queue[100];
+    int front = 0, rear = 0;
+    queue[rear++] = root;
+    
+    while(front<rear){
+        struct node *temp = queue[front++];
+        
+        if(temp->left == NULL){
+            temp->left = new_node;
+            break;
+        }
+        else{
+            queue[rear++] = temp->left;
+        }
+        
+        if(temp->right == NULL){
+            temp->right = new_node;
+            break;
+        }
+        else{
+            queue[rear++] = temp->right;
+        }
+    }
+    
+    return root;
+}
+
+
+void forwardinorder(struct node* root){
+    if(root == NULL){
+        return;
+    }
+    forwardinorder(root->left);
+    printf("%d ", root->data);
+    forwardinorder(root->right);
+}
+
+void reversedinorder(struct node* root){
+    if(root == NULL){
+        return;
+    }
+    reversedinorder(root->right);
+    printf("%d ", root->data);
+    reversedinorder(root->left);
+}
+
+
+int main(){
+    int n, data;
+    struct node *root = NULL;
+    scanf("%d",&n);
+    for(int i = 0 ; i < n ; i++){
+        scanf("%d",&data);
+        root = insert(root, data);
+    }
+    forwardinorder(root);
+    printf("\n");
+    reversedinorder(root);
+    
+}
