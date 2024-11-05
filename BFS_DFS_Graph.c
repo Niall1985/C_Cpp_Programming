e54a406f118f629b0e5c0f4f89cb1895de58eabf
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2024-11-05 21:30:08 +0530

    Update BFS_DFS_Graph.c

--- BFS_DFS_Graph.c
+++ BFS_DFS_Graph.c
@@ -62,3 +62,47 @@ int main(){
     
     dfs(start);
 }
+
+
+
+// dfs implementation
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX 100
+void dfs(int graph[MAX][MAX], int V, bool visited[], int level[], int src, int currentLevel){
+  visited[src] = true;
+  level[src] = currentLevel;
+  printf("%d ", src);
+  for(int i = 0 ; i < V ; i++){
+    if(graph[src][i] && !visited[i]){
+      dfs(graph, V, visited, level, i, currentLevel+1);
+    }
+  }
+}
+
+int main(){
+  int V, E;
+  scanf("%d %d", &V, &E);
+  int graph[MAX][MAX] = {0};
+  for(int i = 0 ; i < E ; i++){
+    int u,v;
+    scanf("%d %d", &u, &v);
+    graph[u][v] = 1;
+    graph[v][u] = 1;
+  }
+  bool visited[MAX] = { false };
+  int level[MAX];
+  for(int i = 0 ; i < V ; i++){
+    level[i] = -1;
+  }
+  int src;
+  scanf("%d", &src);
+  dfs(graph, V, visited, level, src, 0);
+  printf("\n");
+  for(int i = 0 ; i < V ; i++){
+    if(visited[i]){
+      printf("%d - %d\n", i, level[i]);
+    }
+  }
+}
