eaf1048716dcb1c546509ecee3eab41b447ccadb
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2024-11-18 18:50:11 +0530

    Update page_repalcement_fifo.c

--- Os/page_repalcement_fifo.c
+++ Os/page_repalcement_fifo.c
@@ -6,7 +6,7 @@ int main() {
     pages = sizeof(incomingStream) / sizeof(incomingStream[0]);
 
     int temp[frames];
-    for (int i = 0; i < frames; i++) temp[i] = -1;
+    for (int i = 0; i < frames; i++) temp[i] = -1;C
 
     printf("Incoming\tFrame 1\t\tFrame 2\t\tFrame 3\n");
 
@@ -40,3 +40,19 @@ int main() {
     printf("Total Page Faults:\t%d\n", pageFaults);
     return 0;
 }
+
+
+
+
+/*
+Algorithm:
+1. Create an array containing all the page requests and get the size of the array
+2. Create an array of frames based on the size required
+3. Create a variable which counts the page count and set it to zero
+4. Run a loop to initialize all the frames in the frame array to -1
+5. run a loop and iterate over all the page requests
+6. Set a flag and initialize it to 0
+7. Run another loop and iterate over the frames
+8. If we find a hit (req in the page list is in a frame), set the flag to 1 and break n exit the inner loop;
+9. If the flag is not set, we put the incoming pages in the frame in fifo manner -> frames[pagefault % frame size] = page[i] and then increment the count of the page fault
+*/
