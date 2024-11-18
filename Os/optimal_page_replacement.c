ce62821f0c6f79431c4b0761eb6b5fa27afba4ec
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2024-11-18 19:13:56 +0530

    Update optimal_page_replacement.c

--- Os/optimal_page_replacement.c
+++ Os/optimal_page_replacement.c
@@ -1,78 +1,95 @@
 #include <stdio.h>
 
 int main() {
-    int no_of_frames, no_of_pages, frames[10], pages[30], future[10], i, j, k, pos, faults = 0;
-    printf("Enter number of frames: ");
-    scanf("%d", &no_of_frames);
-    printf("Enter number of pages: ");
-    scanf("%d", &no_of_pages);
-    printf("Enter page reference string: ");
-    for (i = 0; i < no_of_pages; i++) {
-        scanf("%d", &pages[i]);
-    }
+    int pages[] = {4, 1, 2, 4, 5};
+    int page_size = sizeof(pages) / sizeof(pages[0]);
+    int frame_size = 3;
+    int frames[frame_size];
+    int pagefault = 0;
 
-    // Initialize frames with -1 (indicating empty)
-    for (i = 0; i < no_of_frames; i++) {
+    // Initialize frames with -1 to indicate they are empty
+    for (int i = 0; i < frame_size; i++) {
         frames[i] = -1;
     }
 
-    for (i = 0; i < no_of_pages; i++) {
+    printf("Incoming\tFrame 1\t\tFrame 2\t\tFrame 3\n");
+
+    for (int i = 0; i < page_size; i++) {
         int found = 0;
-        
-        // Check if the current page is already in a frame
-        for (j = 0; j < no_of_frames; j++) {
+
+        // Check if the page is already in the frame
+        for (int j = 0; j < frame_size; j++) {
             if (frames[j] == pages[i]) {
-                found = 1;
+                found = 1;  // Page is already in one of the frames
                 break;
             }
         }
 
-        // If page is not found in frames, page fault occurs
         if (!found) {
-            faults++;
-            int empty_found = 0;
+            int replace_index = -1;
+            int farthest = -1;
 
-            // Look for an empty frame
-            for (j = 0; j < no_of_frames; j++) {
-                if (frames[j] == -1) {
-                    frames[j] = pages[i];
-                    empty_found = 1;
+            // Find the page to replace (optimal replacement)
+            for (int j = 0; j < frame_size; j++) {
+                int next_use = -1;
+
+                // Find when the current frame will be used next in the future
+                for (int k = i + 1; k < page_size; k++) {
+                    if (frames[j] == pages[k]) {
+                        next_use = k;
+                        break;
+                    }
+                }
+
+                // If the page is not found again in the future, it is the optimal candidate to replace
+                if (next_use == -1) {
+                    replace_index = j;
                     break;
                 }
+
+                // Track the farthest next usage
+                if (next_use > farthest) {
+                    farthest = next_use;
+                    replace_index = j;
+                }
             }
 
-            // If no empty frame, find the optimal page to replace
-            if (!empty_found) {
-                for (j = 0; j < no_of_frames; j++) {
-                    future[j] = -1;
-                    for (k = i + 1; k < no_of_pages; k++) {
-                        if (frames[j] == pages[k]) {
-                            future[j] = k;
-                            break;
-                        }
-                    }
-                }
-
-                // Find the frame with the farthest future use or not used at all
-                pos = 0;
-                for (j = 1; j < no_of_frames; j++) {
-                    if (future[j] == -1 || (future[pos] != -1 && future[j] > future[pos])) {
-                        pos = j;
-                    }
-                }
-                frames[pos] = pages[i];
-            }
+            // Replace the page at the selected frame
+            frames[replace_index] = pages[i];
+            pagefault++;
         }
 
-        // Print the current frame state
+        // Print the current state of frames
+        printf("%d\t\t", pages[i]);
+        for (int j = 0; j < frame_size; j++) {
+            if (frames[j] != -1) {
+                printf("%d\t\t", frames[j]);
+            } else {
+                printf("-\t\t");
+            }
+        }
         printf("\n");
-        for (j = 0; j < no_of_frames; j++) {
-            if (frames[j] != -1)
-                printf("%d\t", frames[j]);
-            else
-                printf("-\t");
-        }
     }
-    printf("\n\nTotal Page Faults = %d\n", faults);
+
+    printf("Page Faults: %d\n", pagefault);
     return 0;
 }
+
+
+/*
+Algorithm:
+1. Create an array containing all the page requests and get the size of the array
+2. Create an array of frames based on the size required
+3. Create a variable which counts the page count and set it to zero
+4. Run a loop to initialize all the frames in the frame array to -1
+5. run a loop and iterate over all the page requests
+6. Set a flag and initialize it to 0
+7. Run another loop and iterate over the frames
+8. If we find a hit (req in the page list is in a frame), set the flag to 1 and break n exit the inner loop;
+9. If the flag is not set, we create two vars int replaced_index (to track the index to be replaced) and int farthest (to track the farthest element) and set their values to -1
+10. Run a loop from 0 to frame_size and we initialize a variable to track the index of when an element in the frame will occur next in the paging sequence
+11. Run a loop from i+1 to page_size and iterate over all the elements, if we find an element that is in the frame and in the request sequnce, we set the next_use variable with the index of the found value and we break
+12. If we don't find such an element, we set the replaced_index value to the current frame index and we break
+13. If we do find a value(we will check if the next_uze val is greater than furthest), if yes then we will set farthest with next_use and replaced_index to the current frame index
+14. The we will fill the page in the frame usinf frame[replaced_index] = page[i] and we will increment the page fault counter
+*/
