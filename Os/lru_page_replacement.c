635c4070354a56392400d1a8fd557ab77fa68958
Niall Dcunha <152503679+Niall1985@users.noreply.github.com>
2024-11-18 19:03:07 +0530

    Update lru_page_replacement.c

--- Os/lru_page_replacement.c
+++ Os/lru_page_replacement.c
@@ -47,3 +47,21 @@ int main(){
   }
     printf("Page Fault: %d", pagefault);
 }
+
+
+/*
+Algorithm:
+1. Create an array containing all the page requests and get the size of the array
+2. Create an array of frames based on the size required
+3. Create a variable which counts the page count and set it to zero
+4. Create a time array with the same size as the frame array to keep track of the access time 
+5. Run a loop to initialize all the frames in the frame array to -1 and all slots in the time array to 0
+6. run a loop and iterate over all the page requests
+7. Set a flag and initialize it to 0
+8. Run another loop and iterate over the frames
+9. If we find a hit (req in the page list is in a frame), set the flag to 1 , set the value of i to the time array for the particular request and break n exit the inner loop;
+10. If the flag is not set, we create a var to track the index of the lru element and set it to 0 as of now.
+11. The we run a loop over all the frames and check if the time[j] < time[lru_time]
+12. If yes, then we set the value of j to the lru_index
+13. Then we put the incoming page request at the lru index in the frame, set the time[lru_index] to i and increment the pade fault count
+*/
