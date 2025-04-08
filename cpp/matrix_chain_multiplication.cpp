b2958e7508e75874d14981b0b323cc735bc45453
Niall1985 <dcunhaniall@gmail.com>
2025-04-08 21:13:10 +0530

    modified mmc

--- cpp/matrix_chain_multiplication.cpp
+++ cpp/matrix_chain_multiplication.cpp
@@ -16,6 +16,22 @@ void printOptimalParens(vector<vector<int>>& s, int i, int j) {
     cout << ")";
 }
 
+
+/* print ((A(BC))D) kinda format
+char character = 'A';
+void PrintParenthesis(vector<vector<int>>&s, int i, int j){
+  if(i == j){
+    cout<<character++;
+    return;
+  }
+  
+  cout<<"(";
+  PrintParenthesis(s, i, s[i][j]);
+  PrintParenthesis(s, s[i][j] + 1, j);
+  cout<<")";
+}
+*/
+
 int main() {
     vector<int> arr = {5, 4, 6, 2, 7}; // Dimensions
     int n = arr.size() - 1; // Number of matrices
